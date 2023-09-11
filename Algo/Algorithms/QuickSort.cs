22d07673d2e5e4c8a85aeb1fe4ca11de9f4bd692
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-09-11 21:27:48 +0500

    Add QuickSort class

---
+++ Algo/Algorithms/QuickSort.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo.Algorithms
+{
+    public class QuickSort : IAlgorithm<int[]>
+    {
+        public void Execute(int[] input)
+        {
+            Sort(input, 0, input.Length - 1);
+        }
+
+        private void Sort(int[] input, int startIndex, int endIndex)
+        {
+            if (endIndex <= startIndex)
+            {
+                return;
+            }
+
+            int pivotIndex = Partition(input, startIndex, endIndex);
+            Sort(input, startIndex, pivotIndex - 1);
+            Sort(input, pivotIndex + 1, endIndex);
+        }
+
+        private int Partition(int[] input, int startIndex, int endIndex)
+        {
+            int pivot = input[endIndex];
+            int i = startIndex - 1;
+
+            for (int j = startIndex; j <= endIndex - 1; j++)
+            {
+                if (input[j] < pivot)
+                {
+                    i++;
+                    Swap(input, i, j);
+                }
+            }
+
+            i++;
+            Swap(input, i, endIndex);
+
+            return i;
+        }
+
+        private void Swap(int[] input, int i, int j)
+        {
+            int temp = input[i];
+            input[i] = input[j];
+            input[j] = temp;
+        }
+    }
+}
