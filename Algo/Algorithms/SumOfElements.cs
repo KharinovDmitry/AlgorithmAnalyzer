290d4093c835060acbf42ce4e49f8fba32170308
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-09-11 19:24:29 +0500

    Add SumOfElements class

---
+++ Algo/Algorithms/SumOfElements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo.Algorithms
+{
+    public class SumOfElements : IAlgorithm<int[]>
+    {
+        public void Execute(int[] input)
+        {
+            var sum = 0;
+
+            foreach (int number in input)
+            {
+                sum += number;
+            }
+        }
+    }
+}
