8fb8e559931a86300d7c32468a01a192746d4d18
vadim471 <tissen.vadimka@mail.ru>
2023-09-18 03:28:25 +0500

    4 алгоритм

--- Algo/Algorithms/Exponentiate4.cs
+++ Algo/Algorithms/Exponentiate4.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Algo.Algorithms
 {
-    internal class Exponentitate4
+    public class Exponentiate4 : IAlgorithm<Tuple<int, int>>
     {
+        public void Execute(Tuple<int, int> input)
+        {
+            Exponentiate(input.Item1, input.Item2);
+        }
+
+        public static int Exponentiate(int value, int pow)
+        {
+            int c = value;
+            int k = pow;
+            int result = 1;
+
+            while (k != 0)
+            {
+                if (k % 2 == 0)
+                {
+                    c *= c;
+                    k /= 2;
+                }
+                else
+                {
+                    result *= c;
+                    k -= 1;
+                }
+            }
+            return result;
+        }
     }
 }
