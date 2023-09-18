cd31ed1eb4711ffcce5f9ab5b5a620fa7181e044
vadim471 <tissen.vadimka@mail.ru>
2023-09-18 19:07:57 +0500

    3 пункт

--- Algo/Algorithms/DoubleNull.cs
+++ Algo/Algorithms/DoubleNull.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Algo.Algorithms
 {
-    internal class DoubleNull
+    public class DoubleNull : IAlgorithm<int[]>
     {
+        public void Execute(int[] x)
+        {
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] == 0)
+                {
+                    for (int j = x.Length - 2; j >= i; j--)
+                    {
+                        x[j + 1] = x[j];
+
+                    }
+                    i++;
+
+                }
+
+            }
+        }
     }
 }
