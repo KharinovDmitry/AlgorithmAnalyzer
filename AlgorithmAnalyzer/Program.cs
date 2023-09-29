5dbb4f57ec88979f35ae4c3d9118878ed8d596d5
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-09-29 22:10:24 +0500

    Add TravellingSalesman class and generating random graph

--- AlgorithmAnalyzer/Program.cs
+++ AlgorithmAnalyzer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Algo;
 using Algo.Algorithms;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 namespace AlgorithmAnalyzer
@@ -18,7 +19,6 @@ namespace AlgorithmAnalyzer
                 new MultiplicationOfElements(),
                 new Polynomial(),
                 new QuickSort(),
-                new ArithmeticMean(),
                 new GornerMethod()
 
             };
@@ -30,10 +30,15 @@ namespace AlgorithmAnalyzer
             {
                 new MultiplyMatrix()
             };
-            List<AnalyzeResult> results = new List<AnalyzeResult> ();
+            List<IAlgorithm<int[,]>> algorithmsOfConnectedGraphs = new List<IAlgorithm<int[,]>>
+            {
+                new TravellingSalesman()
+            };
+            List<AnalyzeResult> results = new List<AnalyzeResult>();
             results.AddRange(algorithmsOfIntArray.Select(x => Analyzer.Evaluate(x)).ToList());
             results.AddRange(algorithmsPower.Select(x => Analyzer.Evaluate(x)).ToList());
             results.AddRange(algorithmsOfMatrix.Select(x => Analyzer.Evaluate(x)).ToList());
+            results.AddRange(algorithmsOfConnectedGraphs.Select(x => Analyzer.Evaluate(x)).ToList());
             results.ForEach(x => CsvWriter.WriteAnalyzeResult(x));
         } 
     }
